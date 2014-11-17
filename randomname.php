bfc34f6061821666ef806c1185c37e985fdcaaa3
mehmet kağan akpınar <kagan.akpinar@bil.omu.edu.tr>
2014-11-17 15:44:54 +0200

    kelime üretemezse sonlandırsın

--- randomname.php
+++ randomname.php
@@ -4,51 +4,70 @@ class randomname{
 	protected $engWords;
    	protected $engAdj;
 
-    	protected $turWords;
-    	protected $turAdj;
-    	protected $colorClass;
-    	protected $argv;
+    protected $turWords;
+    protected $turAdj;
+    protected $colorClass;
+    protected $argv;
+	
 	public function __construct($argv){
-        	$this->argv = $argv;
+        $this->argv = $argv;
 
-        	$this->turWords = array('ev', 'araba', 'elma', 'karpuz', 'iş', 'para');
-       	 	$this->turAdj = array('çok', 'büyük', 'tatlı', 'sarı', 'zengin', 'zevkli');
+        $this->turWords = array('ev', 'araba', 'elma', 'karpuz', 'iş', 'para');
+       	$this->turAdj = array('çok', 'büyük', 'tatlı', 'sarı', 'zengin', 'zevkli');
 
-        	$this->engWords = array('home', 'car', 'apple', 'computer', 'biycle', 'phone');
-        	$this->engAdj = array('heavy', 'yellow', 'expensive', 'big', 'red');
+        $this->engWords = array('home', 'car', 'apple', 'computer', 'biycle', 'phone');
+        $this->engAdj = array('heavy', 'yellow', 'expensive', 'big', 'red');
 
-        	$this->run();
-    	}
-    	function compress($name, $attribute){
-        	return $name . " " . $attribute;
+        $this->run();
+    }
+    function compress($name, $attribute){
+        return $name . " " . $attribute;
    	}	
 	function createDirectory($language, $dirName){
-        	if(!is_dir('dirs'))
-            		mkdir('dirs');
+        if(!is_dir('dirs'))
+            mkdir('dirs');
 
-        	if(!is_dir('dirs/' . $language))
-            		mkdir('dirs/' . $language);
+		if(!is_dir('dirs/' . $language))
+            mkdir('dirs/' . $language);
 
-       	 	if(is_dir('dirs/' . $language . "/" . $dirName))
-           		return false;
-        	mkdir('dirs/' . $language . "/" . $dirName);
-        	echo "Oluşturulan Dizin : ".$dirname."\n\n";
+       	 if(is_dir('dirs/' . $language . "/" . $dirName))
+           	return false;
+        mkdir('dirs/' . $language . "/" . $dirName);
+        echo "Oluşturulan Dizin : ".$dirname."\n\n";
 
-        	return true;
-    	}
+        return true;
+    }
+	
+	function remainingTime($start)
+    {
+        $sure_bitimi = microtime(true);
+        return $sure_bitimi - $start;
+    }
 
-    	function workProgress($language, $words, $adjectives, $sayi){
-        	$sizeOfDir = $sayi;
-        	for(;;){
-            		$randWords = rand(0, sizeof($words) - 1);
-            		$randAdjectives = rand(0, sizeof($adjectives) - 1);
-           		$dirName = $this->compress($words[$randWords], $adjectives[$randAdjectives]);
-            		if($this->createDirectory($language, $dirName)){
-                		$sizeOfDir -= 1;
-           		}	
-        	}
-    	}
+    function workProgress($language, $words, $adjectives, $sayi){
+        $sizeOfDir = $sayi;
+		$maxTimeSeconds = 10;
+		$startTime = microtime(true);
+			
+        for(;;){
+			/*10 saniye bekleyip kelime üretemezse 
+			*program sonlandıracak
+			*/
+			if($this->remainingTime($startTime) >= $maxTimeSeconds){
+				echo $maxTimeSeconds . " saniye süre geçti. Bu sürede oluşturulabilecek dosya ismi bulunamadı" . "\n";
+				break;
+			}
 
-
-
+			if(!$sizeOfDir){
+				break;
+			}
+	
+            $randWords = rand(0, sizeof($words) - 1);
+            $randAdjectives = rand(0, sizeof($adjectives) - 1);
+           	$dirName = $this->compress($words[$randWords], $adjectives[$randAdjectives]);
+            if($this->createDirectory($language, $dirName)){
+               	$sizeOfDir -= 1;
+           	}	
+        }
+    }
 }
