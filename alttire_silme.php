dff40681e70f131e4aab16d4bb400a31f6d1f38b
brkara <burak.kara@bil.omu.edu.tr>
2014-11-18 04:50:31 +0200

    fonksiyonlar eklendi

--- alttire_silme.php
+++ alttire_silme.php
@@ -2,11 +2,21 @@
 
 
 class alttire{
-	protected $arguman;
 	protected $argv;
+	
 	public function __construct($argv){
         $this->argv = $argv;
         $this->calis();
+    }
+	function alttire_ekle($girdi){
+        $ekli = str_replace(' ', '_', $girdi);
+		echo $ekli . '\n';
+    }
+	
+    function alttire_sil($girdi){
+        $degis = str_replace('_', ' ', $girdi);
+		echo $degis . '\n';
+		
     }
 	public function calis(){
 		$argumanlar = $this->argv;
@@ -20,6 +30,9 @@ class alttire{
         elseif(in_array($arg0, array'ekle', 'add', 'olustur'), true)){
             $this->alttire_ekle($arg1);
         }
+		else{
+			echo "Lütfen 'sil' , 'del', 'kaldir', 'ekle', 'add', 'olustur' kelimelerini ve bir \" \" arasýnda cümleyi argüman olarak veriniz arguman veriniz" . '\n';
+		}
 	
 	}
 }
