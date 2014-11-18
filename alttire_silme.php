1d831d0b7f82a301b829bbed08bf9c9d2d1f10d4
brkara <burak.kara@bil.omu.edu.tr>
2014-11-18 04:38:39 +0200

    girdiye göre alttire ekleme veya çıkarma fonksiyonlarının çalıştırılması

--- alttire_silme.php
+++ alttire_silme.php
@@ -8,5 +8,18 @@ class alttire{
         $this->argv = $argv;
         $this->calis();
     }
+	public function calis(){
+		$argumanlar = $this->argv;
+		$arg0 = $arguments[1];
+		$arg1 = $arguments[2];
+
+
+        if(in_array($arg0, array('sil', 'del', 'kaldir'), true)){
+            $this->alttire_sil($arg1);
+        }
+        elseif(in_array($arg0, array'ekle', 'add', 'olustur'), true)){
+            $this->alttire_ekle($arg1);
+        }
 	
+	}
 }
